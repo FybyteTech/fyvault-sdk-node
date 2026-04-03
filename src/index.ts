24fc98fe9f0243bf59947598e8b2b0abd1aff977
fybyte <fybytetech@gmail.com>
2026-04-04 00:57:29 +0530

    docs: add landing page overhaul plan, minor SDK and lockfile cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { FyVault } from "./client.js";
 export { FyVaultError } from "./errors.js";
 export type { FyVaultOptions } from "./client.js";
 export type * from "./types.js";
-export type { AccessTokensResource } from "./resources/accessTokens.js";
